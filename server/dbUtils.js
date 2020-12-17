3697a24eddf4e50d43026803148e5014f6f91e4d
Reetik Patel <Reetik2000@yahoo.com>
2020-12-16 20:36:16 -0600

    🚧 🗃️ Basic locker initialization

--- server/dbUtils.js
+++ server/dbUtils.js
@@ -1,15 +1,24 @@
+// Libraries
 const sqlite3 = require('sqlite3').verbose();
+const lockers = require('./config.json').lockers;
 const DB_PATH = './locker.db';
 
+// Constants
 const createTable = `
     CREATE TABLE IF NOT EXISTS locker (
-        id integer NOT NULL PRIMARY KEY,
-        checked_out boolean NOT NULL,
-        user text,
-        pass text
+        id TINYTEXT NOT NULL PRIMARY KEY,
+        checked_out BOOLEAN NOT NULL,
+        user TINYTEXT,
+        pass SMALLINT
     );
 `;
 
+const createLocker = `
+    REPLACE INTO locker
+    VALUES 
+`;
+
+// Initialization
 const DB = new sqlite3.Database(DB_PATH, function(err) {
     if(err) {
         console.error(err);
@@ -19,6 +28,34 @@ const DB = new sqlite3.Database(DB_PATH, function(err) {
     DB.exec(createTable, function(err) {
         if(err) {
             console.error("Failed to create locker database: ", err);
+            return;
         }
+        
+        var query = createLocker;
+        var lockerQueries = []
+        for(var locker of lockers) {
+            lockerQueries.push(getInitLocker(locker));
+        }
+        query += lockerQueries.join(",");
+        DB.exec(query, function(err) {
+            if(err) {
+                console.error("Failed to generate lockers within database: ", err);
+            }
+        });
     });
 });
+
+// Internal Utilities
+function getInitLocker(locker) {
+    var newLocker = [];
+    for(var k = 1; k <= locker.size; k++) {
+        newLocker.push(`(
+            "${locker.id + k}",
+            ${false},
+            ${null},
+            ${null}
+        )`);
+    }
+
+    return newLocker.join(", ");
+}
