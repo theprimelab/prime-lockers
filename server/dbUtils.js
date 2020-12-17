ffd58183deb424a21dd3203f41648222dbd7ab4a
Reetik Patel <Reetik2000@yahoo.com>
2020-12-16 21:22:24 -0600

    🚧🗃️♻️ Reorganized dbUtils initilation

--- server/dbUtils.js
+++ server/dbUtils.js
@@ -4,49 +4,94 @@ const lockers = require('./config.json').lockers;
 const DB_PATH = './locker.db';
 
 // Constants
-const createTable = `
-    CREATE TABLE IF NOT EXISTS locker (
-        id TINYTEXT NOT NULL PRIMARY KEY,
-        checked_out BOOLEAN NOT NULL,
-        user TINYTEXT,
-        pass SMALLINT
-    );
-`;
+var DB;
 
-const createLocker = `
-    REPLACE INTO locker
-    VALUES 
-`;
-
-// Initialization
-const DB = new sqlite3.Database(DB_PATH, function(err) {
-    if(err) {
-        console.error(err);
-        return;
-    }
-    console.log("Connected to " + DB_PATH + " database.");
-    DB.exec(createTable, function(err) {
-        if(err) {
-            console.error("Failed to create locker database: ", err);
-            return;
+// ----------------------------------------------
+// Initializers
+// ----------------------------------------------
+/**
+ * Initializes the locker database
+ * @returns {Promise}
+ */
+function init() {
+    return new Promise(function(resolve, reject) {
+        if(!lockers) {
+            reject();
         }
-        
-        var query = createLocker;
+
+        DB = new sqlite3.Database(DB_PATH, async function(err) {
+            if(err) {
+                console.error(err);
+                reject(err);
+            }
+            console.log("Connected to database at " + DB_PATH);
+
+            await initLockerTable().catch(function(err) {
+                reject(err);
+            });
+            await initLockerRows().catch(function(err) {
+                reject(err);
+            });
+
+            console.log("Locker DB Ready!");
+            resolve();
+        });
+    });
+}
+
+/**
+ * Initializes Locker table
+ * @returns {Promise}
+ */
+function initLockerTable() {
+    return new Promise(function(resolve, reject) {
+        const createTable = `
+            CREATE TABLE IF NOT EXISTS locker (
+                id TINYTEXT NOT NULL PRIMARY KEY,
+                checked_out BOOLEAN NOT NULL,
+                user TINYTEXT,
+                pass SMALLINT
+            );
+        `;
+        DB.exec(createTable, function(err) {
+            if(err) {
+                console.error("Failed to create locker database: ", err);
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+/**
+ * Initializes Locker table
+ * @returns {Promise}
+ */
+function initLockerRows() {
+    return new Promise(function(resolve, reject) {
+        var query = "REPLACE INTO locker VALUES";
         var lockerQueries = []
         for(var locker of lockers) {
-            lockerQueries.push(getInitLocker(locker));
+            lockerQueries.push(generateLocker(locker));
         }
         query += lockerQueries.join(",");
+
         DB.exec(query, function(err) {
             if(err) {
                 console.error("Failed to generate lockers within database: ", err);
+                reject(err);
             }
+
+            resolve();
         });
     });
-});
+}
 
-// Internal Utilities
-function getInitLocker(locker) {
+/**
+ * Generates a SQL value string with rows for a locker set
+ * @param {String} locker 
+ */
+function generateLocker(locker) {
     var newLocker = [];
     for(var k = 1; k <= locker.size; k++) {
         newLocker.push(`(
